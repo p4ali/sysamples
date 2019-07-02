66d597abc9fe4d8c15484630b25946b905a25bd7
Jianfei Hu <jianfeih@google.com>
2019-07-02 00:55:13 +0000

    some shell improv.

--- envoy/listener-stuck/README.md
+++ envoy/listener-stuck/README.md
@@ -1,3 +1,8 @@
 # Listener Stuck
 
 https://github.com/envoyproxy/envoy/issues/6904
+
+Desired state
+
+- 9080 is not stuck, 9943 is stuck, fine...
+- configudump active listeners is correct.
--- envoy/listener-stuck/setup.sh
+++ envoy/listener-stuck/setup.sh
@@ -1,18 +1,16 @@
 #DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ENVOY_BIN="$HOME/workspace/github.com/envoyproxy/envoy/bazel-bin/source/exe/envoy-static"
 
-#copyconfig() {
-  #rm -rf /tmp/envoy
-  #mkdir -p /tmp/envoy
-  #ln ./config_not_warm.yaml /tmp/envoy/config_not_warming.yaml
-  #ln ./listeners-1-ready.yaml /tmp/envoy/
-  #ln ./listeners-2-sds.yaml /tmp/envoy/
-  ## ln $DIR/listen /tmp/envoy/listeners_not_warming.yaml
-#}
-
-fakeserver() {
+fakesds() {
+  echo 'fakesds'
   nc -l -p 9903
 }
+
+fakeserver() {
+  echo 'fakeserver'
+  nc -l -p 9902
+}
+
 
 startenvoy() {
   opt="${1:-ready.yaml}"
@@ -24,4 +22,9 @@ startenvoy() {
 check() {
   echo "Check config dump...."
   curl -s 127.0.0.1:9901/config_dump | jq -r ".configs[].dynamic_active_listeners[]? | .listener.address"
+}
+
+startsetup() {
+  tmux split-window 'source setup.sh && fakesds'
+  tmux split-window 'source setup.sh && fakeserver'
 }
